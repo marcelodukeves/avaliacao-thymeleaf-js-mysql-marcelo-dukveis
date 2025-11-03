f09eba2f0eb01fb61e8130b076f66911a40a5aa2
marcelodukeves <75148111+marcelodukeves@users.noreply.github.com>
2025-11-03 20:48:07 -0300

    commit corrigido
    corrigido entidades e criado banco de dados versão 3

--- Senac-Spring-MySQL-master/Senac-Spring-MySQL-master/src/main/java/com/senac/crud/model/Pessoa.java
+++ Senac-Spring-MySQL-master/Senac-Spring-MySQL-master/src/main/java/com/senac/crud/model/Pessoa.java
@@ -5,14 +5,11 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-
-@javax.persistence.Entity
+@Entity
 public class Pessoa {
 
-    @javax.persistence.Id
-    @com.senac.crud.model.GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String nome;
